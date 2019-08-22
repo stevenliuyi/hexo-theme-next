98e30300ca2e57fcdf28f07c8e746ad9c22e920b
stevenliuyi <stevenliuyi@gmail.com>
2019-08-22 16:04:37 -0400

    minor text change

--- scripts/tags/zhihu-info.js
+++ scripts/tags/zhihu-info.js
@@ -3,7 +3,7 @@
 'use strict';
 
 function zhihuInfo(args) {
-  return '<div class="article-info">注：本文首发于知乎，为问题 <a href="https://www.zhihu.com/' + args[1] + '">' + args[0] + '</a> 的回答。</div>'
+  return '<div class="article-info">注：本文首发于知乎问题 <a href="https://www.zhihu.com/' + args[1] + '">' + args[0] + '</a> 。</div>'
 }
 
 hexo.extend.tag.register('zhihuinfo', zhihuInfo, { ends: false });
