39122fcf8aca1f6888f0973127d8958d16cc1e65
stevenliuyi <stevenliuyi@gmail.com>
2019-08-22 20:20:25 -0400

    remove the period

--- scripts/tags/zhihu-info.js
+++ scripts/tags/zhihu-info.js
@@ -3,7 +3,7 @@
 'use strict';
 
 function zhihuInfo(args) {
-  return '<div class="article-info">注：本文首发于知乎问题 <a href="https://www.zhihu.com/' + args[1] + '">' + args[0] + '</a> 。</div>'
+  return '<div class="article-info">注：本文首发于知乎问题 <a href="https://www.zhihu.com/' + args[1] + '">' + args[0] + '</a></div>'
 }
 
 hexo.extend.tag.register('zhihuinfo', zhihuInfo, { ends: false });
